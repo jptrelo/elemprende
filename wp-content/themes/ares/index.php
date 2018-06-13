5f0efb34e71d60776f79040657e124e1ddb616c5
Jhon Trejos <jhon.trejos@qrvey.com>
2018-06-13 00:48:58 -0500

    fix empresas

--- wp-content/themes/ares/index.php
+++ wp-content/themes/ares/index.php
@@ -26,93 +26,92 @@ $alternate_blog = isset( $ares_options['blog_layout_style'] ) && $ares_options['
         <div class="container">
 
             <div class="page-content row">
+
+                <!-- <div class="col-md-<?php echo $ares_options['ares_blog_layout'] == 'col2r' && is_active_sidebar(1) ? '8' : '12'; ?> site-content item-page"> -->
                 <div class="col-md-9">
-                    <!-- <div class="col-md-<?php echo $ares_options['ares_blog_layout'] == 'col2r' && is_active_sidebar(1) ? '8' : '12'; ?> site-content item-page"> -->
-                    <div class="col-md-4 col-sm-6">
 
-                        <?php if ( have_posts() ) :
-                        
-                            if ( $alternate_blog ) : ?>
+                    <?php if ( have_posts() ) :
+                    
+                        if ( $alternate_blog ) : ?>
 
-                                <div id="ares-alt-blog-wrap">
+                            <div id="ares-alt-blog-wrap">
 
-                                    <div id="masonry-blog-wrapper">
+                                <div id="masonry-blog-wrapper">
 
-                                        <div class="grid-sizer"></div>
-                                        <div class="gutter-sizer"></div>
+                                    <div class="grid-sizer"></div>
+                                    <div class="gutter-sizer"></div>
 
-                            <?php endif;
-                        
-                            while ( have_posts() ) : the_post();
+                        <?php endif;
+                    
+                        while ( have_posts() ) : the_post();
 
-                                if ( $alternate_blog ) :
-                                        
-                                    get_template_part('template-parts/content', 'posts-alt' );
-                                        
-                                else : ?>
-                                        
-                                    <div class="item-post">
+                            if ( $alternate_blog ) :
+                                    
+                                get_template_part('template-parts/content', 'posts-alt' );
+                                    
+                            else : ?>
+                                    
+                                <div class="item-post col-md-4 col-sm-6">
 
-                                        <?php if ( $ares_options['ares_blog_featured'] == 'on' && has_post_thumbnail() ) : ?>
+                                    <?php if ( $ares_options['ares_blog_featured'] == 'on' && has_post_thumbnail() ) : ?>
 
-                                            <div class="post-thumb col-sm-12">
+                                        <div class="post-thumb col-sm-12">
 
-                                                <a href="<?php the_permalink(); ?>">
-                                                    <?php the_post_thumbnail('large'); ?>
-                                                </a>
-
-                                            </div>
-
-                                        <?php endif; ?>
-
-                                        <!-- <div class="col-sm-<?php echo $ares_options['ares_blog_featured'] == 'on' && has_post_thumbnail() ? '8' : '12'; ?> <?php echo has_post_thumbnail() ? '' : 'text-left'; ?>"> -->
-                                        <div class="col-sm-12">
-
-                                            <h2 class="post-title">
-                                                <a href="<?php the_permalink(); ?>">
-                                                    <?php the_title(); ?>
-                                                </a>
-                                            </h2>
-
-                                            <div class="post-content">
-                                                <?php the_excerpt(); ?>
-                                            </div>
-
-                                            <div class="text-right">
-                                                <a class="ares-button button-primary" href="<?php the_permalink(); ?>">
-                                                    <?php _e( 'Read More', 'ares' ); ?>
-                                                </a>
-                                            </div>  
+                                            <a href="<?php the_permalink(); ?>">
+                                                <?php the_post_thumbnail('large'); ?>
+                                            </a>
 
                                         </div>
 
-                                    </div>
-                                        
-                                <?php endif;
+                                    <?php endif; ?>
 
-                            endwhile;
-                                        
-                            if ( $alternate_blog ) : ?>
+                                    <!-- <div class="col-sm-<?php echo $ares_options['ares_blog_featured'] == 'on' && has_post_thumbnail() ? '8' : '12'; ?> <?php echo has_post_thumbnail() ? '' : 'text-left'; ?>"> -->
+                                    <div class="col-sm-12">
+
+                                        <h2 class="post-title">
+                                            <a href="<?php the_permalink(); ?>">
+                                                <?php the_title(); ?>
+                                            </a>
+                                        </h2>
+
+                                        <div class="post-content">
+                                            <?php the_excerpt(); ?>
+                                        </div>
+
+                                        <div class="text-right">
+                                            <a class="ares-button button-primary" href="<?php the_permalink(); ?>">
+                                                <?php _e( 'Read More', 'ares' ); ?>
+                                            </a>
+                                        </div>  
 
                                     </div>
-                                    
+
                                 </div>
+                                    
+                            <?php endif;
 
-                            <?php endif; ?>
+                        endwhile;
+                                    
+                        if ( $alternate_blog ) : ?>
 
-                            <div class="pagination-links">
-                                <?php echo the_posts_pagination( array( 'mid_size' => 1 ) ); ?>
+                                </div>
+                                
                             </div>
-                        
-                        <?php else : ?>
-                        
-                            <?php get_template_part('template-parts/content', 'none'); ?>
-                        
+
                         <?php endif; ?>
-                        
-                    </div>
+
+                        <div class="pagination-links">
+                            <?php echo the_posts_pagination( array( 'mid_size' => 1 ) ); ?>
+                        </div>
+                    
+                    <?php else : ?>
+                    
+                        <?php get_template_part('template-parts/content', 'none'); ?>
+                    
+                    <?php endif; ?>
                     
                 </div>
+                    
                 <?php if ( $ares_options['ares_blog_layout'] == 'col2r' && is_active_sidebar(1) ) : ?>
 
                     <div class="col-md-3 avenue-sidebar">
