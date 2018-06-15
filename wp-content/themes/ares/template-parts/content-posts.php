0be8eeef0723af94daa482df3213333bde9b8cb3
Jhon Trejos <jhon.trejos@qrvey.com>
2018-06-14 22:33:07 -0500

    categories grid

--- wp-content/themes/ares/template-parts/content-posts.php
+++ wp-content/themes/ares/template-parts/content-posts.php
@@ -8,32 +8,33 @@
 $ares_options = ares_get_options();
 
 ?>
-
-<div class="item-post <?php echo has_post_thumbnail() && $ares_options['ares_blog_featured'] == 'on' ? '' : 'text-left'; ?>">
-    
-    <?php if ( has_post_thumbnail() && $ares_options['ares_blog_featured'] == 'on' ) : ?>
-    
-        <div class="post-thumb col-md-4">
-            <a href="<?php the_permalink(); ?>">
-                <?php the_post_thumbnail('large'); ?>
-            </a>
+<div class="col-md-3 col-sm-3 col-xs-6">
+    <div class="item-post <?php echo has_post_thumbnail() && $ares_options['ares_blog_featured'] == 'on' ? '' : 'text-left'; ?>">
+        
+        <?php if ( has_post_thumbnail() && $ares_options['ares_blog_featured'] == 'on' ) : ?>
+        
+            <div class="post-thumb col-md-12">
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail('large'); ?>
+                </a>
+            </div>
+        
+        <?php endif; ?>
+        
+        <!-- <div class="col-md-<?php echo has_post_thumbnail() && $ares_options['ares_blog_featured'] == 'on' ? '8' : '12'; ?>"> -->
+        <div class="col-md-12">
+            <h2 class="post-title">
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_title(); ?>
+                </a>
+            </h2>
+            <div class="post-content">
+                <?php echo wp_trim_words( $post->post_content, 50); ?>
+            </div>
+            <div class="text-right">
+                <a class="ares-button button-primary" href="<?php the_permalink(); ?>"><?php echo esc_html_e( 'Read More', 'ares' ); ?></a>
+            </div>                        
         </div>
-    
-    <?php endif; ?>
-    
-    <div class="col-md-<?php echo has_post_thumbnail() && $ares_options['ares_blog_featured'] == 'on' ? '8' : '12'; ?>">
-    <!-- <div class="col-md-12"> -->
-        <h2 class="post-title">
-            <a href="<?php the_permalink(); ?>">
-                <?php the_title(); ?>
-            </a>
-        </h2>
-        <div class="post-content">
-            <?php echo wp_trim_words( $post->post_content, 50); ?>
-        </div>
-        <div class="text-right">
-            <a class="ares-button button-primary" href="<?php the_permalink(); ?>"><?php echo esc_html_e( 'Read More', 'ares' ); ?></a>
-        </div>                        
+        
     </div>
-    
 </div>
