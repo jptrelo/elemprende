2efbb4184393ba737af9e690e89f7d377f6c846c
Jhon <jhon@Jhons-MacBook-Pro.local>
2018-09-25 20:39:47 -0500

    wordpress files

--- wp-content/uploads/wpo_wcpdf/fonts/dompdf_font_family_cache.php
+++ wp-content/uploads/wpo_wcpdf/fonts/dompdf_font_family_cache.php
@@ -1,122 +1,122 @@
 <?php return array (
   'sans-serif' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Helvetica',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Helvetica-Bold',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Helvetica-Oblique',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Helvetica-BoldOblique',
+    'normal' => $fontDir . '/Helvetica',
+    'bold' => $fontDir . '/Helvetica-Bold',
+    'italic' => $fontDir . '/Helvetica-Oblique',
+    'bold_italic' => $fontDir . '/Helvetica-BoldOblique',
   ),
   'times' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Times-Roman',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Times-Bold',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Times-Italic',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Times-BoldItalic',
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
   ),
   'times-roman' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Times-Roman',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Times-Bold',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Times-Italic',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Times-BoldItalic',
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
   ),
   'courier' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Courier',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Courier-Bold',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Courier-Oblique',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Courier-BoldOblique',
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
   ),
   'helvetica' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Helvetica',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Helvetica-Bold',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Helvetica-Oblique',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Helvetica-BoldOblique',
+    'normal' => $fontDir . '/Helvetica',
+    'bold' => $fontDir . '/Helvetica-Bold',
+    'italic' => $fontDir . '/Helvetica-Oblique',
+    'bold_italic' => $fontDir . '/Helvetica-BoldOblique',
   ),
   'zapfdingbats' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/ZapfDingbats',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/ZapfDingbats',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/ZapfDingbats',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/ZapfDingbats',
+    'normal' => $fontDir . '/ZapfDingbats',
+    'bold' => $fontDir . '/ZapfDingbats',
+    'italic' => $fontDir . '/ZapfDingbats',
+    'bold_italic' => $fontDir . '/ZapfDingbats',
   ),
   'symbol' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Symbol',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Symbol',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Symbol',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Symbol',
+    'normal' => $fontDir . '/Symbol',
+    'bold' => $fontDir . '/Symbol',
+    'italic' => $fontDir . '/Symbol',
+    'bold_italic' => $fontDir . '/Symbol',
   ),
   'serif' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Times-Roman',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Times-Bold',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Times-Italic',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Times-BoldItalic',
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
   ),
   'monospace' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Courier',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Courier-Bold',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Courier-Oblique',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Courier-BoldOblique',
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
   ),
   'fixed' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Courier',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Courier-Bold',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Courier-Oblique',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Courier-BoldOblique',
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
   ),
   'dejavu sans' => 
   array (
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSans-Bold',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSans-BoldOblique',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSans-Oblique',
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSans',
+    'bold' => $fontDir . '/DejaVuSans-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSans-BoldOblique',
+    'italic' => $fontDir . '/DejaVuSans-Oblique',
+    'normal' => $fontDir . '/DejaVuSans',
   ),
   'dejavu sans mono' => 
   array (
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSansMono-Bold',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSansMono-BoldOblique',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSansMono-Oblique',
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSansMono',
+    'bold' => $fontDir . '/DejaVuSansMono-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSansMono-BoldOblique',
+    'italic' => $fontDir . '/DejaVuSansMono-Oblique',
+    'normal' => $fontDir . '/DejaVuSansMono',
   ),
   'dejavu serif' => 
   array (
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSerif-Bold',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSerif-BoldItalic',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSerif-Italic',
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/DejaVuSerif',
+    'bold' => $fontDir . '/DejaVuSerif-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSerif-BoldItalic',
+    'italic' => $fontDir . '/DejaVuSerif-Italic',
+    'normal' => $fontDir . '/DejaVuSerif',
   ),
   'open sans' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/OpenSans-Normal',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/OpenSans-Bold',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/OpenSans-Italic',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/OpenSans-BoldItalic',
+    'normal' => $fontDir . '/OpenSans-Normal',
+    'bold' => $fontDir . '/OpenSans-Bold',
+    'italic' => $fontDir . '/OpenSans-Italic',
+    'bold_italic' => $fontDir . '/OpenSans-BoldItalic',
   ),
   'segoe' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Segoe-Normal',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Segoe-Bold',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Segoe-Italic',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/Segoe-BoldItalic',
+    'normal' => $fontDir . '/Segoe-Normal',
+    'bold' => $fontDir . '/Segoe-Bold',
+    'italic' => $fontDir . '/Segoe-Italic',
+    'bold_italic' => $fontDir . '/Segoe-BoldItalic',
   ),
   'roboto slab' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/RobotoSlab-Normal',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/RobotoSlab-Bold',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/RobotoSlab-Italic',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/RobotoSlab-BoldItalic',
+    'normal' => $fontDir . '/RobotoSlab-Normal',
+    'bold' => $fontDir . '/RobotoSlab-Bold',
+    'italic' => $fontDir . '/RobotoSlab-Italic',
+    'bold_italic' => $fontDir . '/RobotoSlab-BoldItalic',
   ),
   'currencies' => 
   array (
-    'normal' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/currencies',
-    'bold' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/currencies',
-    'italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/currencies',
-    'bold_italic' => 'C:\\xampp\\htdocs\\emprenet\\wordpress/wp-content/uploads/wpo_wcpdf/fonts/currencies',
+    'normal' => $fontDir . '/currencies',
+    'bold' => $fontDir . '/currencies',
+    'italic' => $fontDir . '/currencies',
+    'bold_italic' => $fontDir . '/currencies',
   ),
 );
 ?>
